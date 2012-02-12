9e071483c9a0c23626388876c91721150bd07b49
Luke Braly <lbraly@wordrad.com>
2012-02-11 23:41:42 -0600

    Heroku Init

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'http://rubygems.org'
+
+gem 'rails'
+
+group :production do
+  gem 'pg'
+end
+
+group :development, :test do
+   gem 'sqlite3'
+end
+
+group :development do
+  gem 'rspec-rails'
+end
+
+group :test do
+  gem 'rspec-rails'
+  gem 'webrat'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'dc97817971bb5a506304eac6310bee00cf1cc1c9ac9ad8626229d4f52639e4a9b2a6bdc9e69d3ce444208052b23cf0fd41680113cd74502c89dcd3ad2a6a9d7d'
